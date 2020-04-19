0014fe3401ff704b04bd6837104690f8e9fcb9ce
Sergey Kolesnikov <sergeykool37@gmail.com>
2020-04-19 11:32:22 +0700

    Оптимизация импорта, удаление new List, удаление недоступного транспорта.

--- unit5/unit5-homework/src/main/java/pro/sisit/javacourse/PathFinder.java
+++ unit5/unit5-homework/src/main/java/pro/sisit/javacourse/PathFinder.java
@@ -1,22 +1,17 @@
 package pro.sisit.javacourse;
 
-import jdk.internal.jline.internal.Nullable;
 import pro.sisit.javacourse.optimal.DeliveryTask;
-import pro.sisit.javacourse.optimal.Route;
 import pro.sisit.javacourse.optimal.RouteType;
 import pro.sisit.javacourse.optimal.Transport;
 
-import java.beans.Expression;
-import java.lang.reflect.Array;
 import java.math.BigDecimal;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static java.math.BigDecimal.valueOf;
-import static java.util.Comparator.*;
-import static pro.sisit.javacourse.optimal.RouteType.Road;
-import static pro.sisit.javacourse.optimal.RouteType.Sea;
+import static java.util.Comparator.comparing;
 
 public class PathFinder {
 
@@ -31,7 +26,7 @@ public class PathFinder {
        Optional<List> transportOptional = Optional.ofNullable(transports);
        if (deliveryTaskOptional.isPresent()&transportOptional.isPresent())
         {
-            ArrayList<Transport> enableTransport=new ArrayList<>(transports
+            ArrayList<Transport> enableTransport=(transports
                     .stream()
                     .filter((transport)-> transport==filterTransport(transport,deliveryTask))
                     .collect(Collectors.toCollection(()->new ArrayList<>())));
@@ -54,6 +49,6 @@ public class PathFinder {
                     .anyMatch(route -> transport.getType() == route.getType() &
                             transport.getVolume().compareTo(deliveryTask.getVolume()) > -1);
             if (result){return transport;}
-            else {return new Transport("Тапки", valueOf(0), valueOf(0), RouteType.Road);}
+            else {return null;}
         }
 }
