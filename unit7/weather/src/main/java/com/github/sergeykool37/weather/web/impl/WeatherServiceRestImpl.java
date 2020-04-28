e3dafa5a1bf338fae2e119c244d4e4f4af69d727
Sergey Kolesnikov <sergeykool37@gmail.com>
2020-04-28 10:29:42 +0700

    Данные API убрал в properties

--- unit7/weather/src/main/java/com/github/sergeykool37/weather/web/impl/WeatherServiceRestImpl.java
+++ unit7/weather/src/main/java/com/github/sergeykool37/weather/web/impl/WeatherServiceRestImpl.java
@@ -2,6 +2,7 @@ package com.github.sergeykool37.weather.web.impl;
 
 import com.github.sergeykool37.weather.dto.WeatherDTO;
 import com.github.sergeykool37.weather.web.WeatherService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -17,13 +18,20 @@ public class WeatherServiceRestImpl implements WeatherService {
     public WeatherServiceRestImpl(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
+    @Value("${x-rapidapi-host}")
+    private String host;
+    @Value("${x-rapidapi-key}")
+    private String keyAPI;
+    @Value("${url}")
+    private String url;
+
 
     @Override
     public String getWeather(String city) {
         MultiValueMap<String,String> headers=new LinkedMultiValueMap<>();
-        String URL= String.format("https://community-open-weather-map.p.rapidapi.com/weather?units=metric&mode=json&q=%s", city);
-        headers.add("x-rapidapi-host","community-open-weather-map.p.rapidapi.com");
-        headers.add("x-rapidapi-key","cdd96ad95amsheebcca53e65aac1p136010jsn18e30e4e6350");
+        String URL= String.format(url, city);
+        headers.add("x-rapidapi-host",host);
+        headers.add("x-rapidapi-key",keyAPI);
         HttpEntity entity=new HttpEntity(headers);
         ResponseEntity<WeatherDTO> result=restTemplate.exchange(URL,HttpMethod.GET,entity, WeatherDTO.class,headers);
         return  result.getBody().getMain().getTemp();
